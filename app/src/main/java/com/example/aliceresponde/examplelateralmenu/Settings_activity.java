b92704781fd8b77908a01059f9e76fe32a645667
Alicia Beltran <aliceresponde@gmail.com>
2016-03-25 15:38:52 -0500

    define settings_activity layout

--- app/src/main/java/com/example/aliceresponde/examplelateralmenu/Settings_activity.java
+++ app/src/main/java/com/example/aliceresponde/examplelateralmenu/Settings_activity.java
@@ -1,5 +1,6 @@
 package com.example.aliceresponde.examplelateralmenu;
 
+
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,7 +10,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class Settings_activity extends AppCompatActivity {
-
     Toolbar toolbar;
 
     @Override
@@ -17,36 +17,35 @@ public class Settings_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_activity);
 
-        toolbar = (Toolbar)  findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         TypedValue typedValueColorPrimaryDark = new TypedValue();
-
-        Settings_activity.this.getTheme().resolveAttribute(R.attr.colorPrimaryDark,
-                typedValueColorPrimaryDark, true);
-
-        final int colorPrimaryDark =typedValueColorPrimaryDark.data;
-
-        if(Build.VERSION.SDK_INT>=21){
+        Settings_activity.this.getTheme().resolveAttribute(R.attr.colorPrimaryDark, typedValueColorPrimaryDark, true);
+        final int colorPrimaryDark = typedValueColorPrimaryDark.data;
+        if (Build.VERSION.SDK_INT >= 21) {
             getWindow().setStatusBarColor(colorPrimaryDark);
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        //inflate menu
-        getMenuInflater().inflate(  R.menu.menu_settings, menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
         return true;
     }
 
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        if (id== R.id.action_settings){
-          return   true;
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
